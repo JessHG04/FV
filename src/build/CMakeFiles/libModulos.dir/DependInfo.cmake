ec0fcc6db2dd91f1edc613424ec2c871ee2572f3
juancarlosjcsm <juancarlossorianom@gmail.com>
2020-03-22 18:26:01 +0100

    personaje y principio menu

---
+++ src/build/CMakeFiles/libModulos.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/Guerrera.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/Guerrera.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/Juego.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/Juego.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/Jugador.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/Jugador.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/Mago.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/Mago.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/Menu.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/Menu.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/Proyectil.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/Proyectil.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/mimodulo.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/mimodulo.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/mimodulo2.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/mimodulo2.cpp.o"
+  "/home/juan/Escritorio/Salto/plantilla-sfml_v2/ej_modulos/spritePersonaje.cpp" "/home/juan/Escritorio/Salto/plantilla-sfml_v2/build/CMakeFiles/libModulos.dir/ej_modulos/spritePersonaje.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ej_modulos"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/ej_modulos/Jugador.cpp
@@ -0,0 +1,11 @@
+#include "Jugador.h"
+
+Jugador::Jugador(){
+    
+}
+
+void Jugador::update(){ //Actuaiza fisicas del jugador
+    //if(movimiento){
+        set_translacion(velocidad);
+    //}
+}
