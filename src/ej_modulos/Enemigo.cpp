c66d98239a49366b6e861bcc37c3f72a55b40047
AlejandroDCastro <acastrovalero@gmail.com>
2020-03-28 13:14:49 -0700

    NPC Darkrai, compilar y crear el ejecutable denuevo

---
+++ src/ej_modulos/Enemigo.cpp
@@ -0,0 +1,22 @@
+#include "Enemigo.h"
+
+Enemigo::Enemigo(int vidas) {
+    this->vidas = vidas;
+}
+
+Enemigo::~Enemigo() {
+
+}
+
+void Enemigo::perderVida(){
+    if(!muerto){
+        vidas--;
+        if(vidas == 0){
+            muerto = true;
+        }
+    }
+}
+
+bool Enemigo::getMuerte(){
+    return muerto;
+}
