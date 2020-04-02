a5d8dddb0f47e216ba06a3832f960409221ea48f
AlejandroDCastro <acastrovalero@gmail.com>
2020-04-02 13:08:42 -0700

    Mejora Upcasting NPC

--- src/ej_modulos/Enemigo.h
+++ src/ej_modulos/Enemigo.h
@@ -9,11 +9,15 @@ using namespace sf;
 
 class Enemigo {
     public:
+        Enemigo();
         Enemigo(int );
         virtual ~Enemigo();
         void perderVida();  
         bool getMuerte();
         int getNumVidas();
+        virtual void recibeGolpe();
+        virtual void Update(float );
+        virtual void Draw(RenderWindow &);
 
     protected:
         int vidas;
