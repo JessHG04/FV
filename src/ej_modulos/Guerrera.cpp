1d5dc5a58373184ed35d5f89d5965e3da40a61a1
JessHG04 <jessicahg04@gmail.com>
2020-05-04 21:00:33 +0200

    Juego casi terminado

--- src/ej_modulos/Guerrera.cpp
+++ src/ej_modulos/Guerrera.cpp
@@ -2,7 +2,7 @@
 
 Guerrera::Guerrera(int cantidadX1, int cantidadY1 , sf::Vector2i frm_act){
     //Inicializo la vida
-    vida = 10;
+    vida = 5;
     archivo = "resources/Sprites/Merche/";
     //spriteJugador = new spritePersonaje();
     set_sprite(txt_player2,cantidadX1, cantidadY1, frm_act);
